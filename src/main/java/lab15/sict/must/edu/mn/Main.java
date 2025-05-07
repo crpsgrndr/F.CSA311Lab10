b4609a453863548e9c6d9187f9dc06a78ee7a257
Usukhbayar Orosoo <euaxlura@yahoo.com>
2025-05-07 17:41:20 +0800

    First commit

---
+++ src/main/java/lab15/sict/must/edu/mn/Main.java
@@ -0,0 +1,7 @@
+package lab15.sict.must.edu.mn;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/lab15/sict/must/edu/mn/MainTest.java
@@ -0,0 +1,11 @@
+package lab15.sict.must.edu.mn;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    void testHello() {
+        assertEquals(2 + 2, 4);  
+    }
+}
